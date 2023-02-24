716c3148a63269b18273267d145d480b6ec745dc
GuizinMEndes2 <126169397+GuizinMEndes2@users.noreply.github.com>
2023-02-24 10:59:50 -0300

    Teste com twig

--- produtos.php
+++ produtos.php
@@ -6,7 +6,27 @@ $loader = new \Twig\Loader\FilesystemLoader('./templates');
 $twig = new \Twig\Environment($loader);
 
 $template = $twig-> load('produtos.html');
+$produtos= [
+[    
+    'nome' => 'Chinelo',
+    'preco' => 30,
+],
+    [
+    'nome' => 'camiseta',
+    'preco' => 50,
+    ],
+    [
+        'nome'=> 'boné',
+        'preco'=> 29.9
+    ],
+    [
+        'nome' => 'lancha',
+        'preco'=> '100000000000'
+    ]
+];
 
 echo $template->render ([
-    'titulo' => 'produtos'
+    'titulo' => 'produtos',
+    //'produtos' => $produtos,
+    'produtos' => null,
 ]);
